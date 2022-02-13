afa3e9671f6b6a877723bd782e29478c696138da
crclaramount <carlos.claramountruiz@telus.com>
2022-02-13 13:14:50 -0800

    Uploading SOAP Services Server Example

--- 03-services/services-101/src/com/telusinternational/classroom/integrations/soap/StringOperations.java
+++ 03-services/services-101/src/com/telusinternational/classroom/integrations/soap/StringOperations.java
@@ -1,16 +1,22 @@
 package com.telusinternational.classroom.integrations.soap;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 
 @WebService
-public class StringOperations {
-	private String message = new String("Hello, ");
+@SOAPBinding(style = Style.RPC)
+public interface StringOperations {
 
-    public void Hello() {}
-
-    @WebMethod
-    public String sayHello(String name) {
-        return message + name + ".";
-    }
+	@WebMethod(operationName = "helloToMe")
+	@WebResult(name = "helloToMeResponseNode", targetNamespace = "http://www.telusinternational.com")
+	String sayHello(@WebParam(name="name") String name);
+	
+	@WebMethod(operationName = "goodbyeToMe")
+	@WebResult(name = "goodbyeToMeResponseNode", targetNamespace = "http://www.telusinternational.com")
+	String sayGoodbye(@WebParam(name="name") String name);
+	
 }
